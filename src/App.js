78559b28cb8af39203609fbd813b39923007654a
Maksym Zyza <mmziza88@gmail.com>
2021-02-28 08:42:14 +0200

    змінена струкрура папок проекту

--- src/App.js
+++ src/App.js
@@ -1,19 +1,69 @@
 import React from 'react';
-// import Counter from "./components /Counter/Counter";
-import Feedback from "./components /Feedback/Feedback";
+import PropTypes from 'prop-types';
+import Controls from './_components/Controls';
 import './styles.css';
+
 
 class App extends React.Component {
   
-  render() {
-    return (
-    <div>
-      {/* <h1>Counter</h1>      
-        <Counter initialValue={10} />  */}
+  static defauitProps = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  }
 
-      <h1>Please leave feedback</h1>      
-        <Feedback /> 
-    </div>
+  static typePrors = {
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+  }
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  }
+
+  handlerFeedback = (e) => {
+    this.setState(prevState => {
+      const btnId = e.target.id;
+      return { [btnId]: prevState[btnId] + 1 };
+     })
+  }
+
+  feedbackCounter = () =>
+    Object.values(this.state).reduce((acc, value) => acc + value, 0);
+  
+  positivePercentageCounter = () => {
+    return Math.round((100 / this.feedbackCounter()) * this.state.good);
+  };
+
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.feedbackCounter();
+    const positivePercentage = this.positivePercentageCounter();
+
+    return (
+      <div className={'Container'}>
+        <h1>Please leave feedback</h1>  
+        <Controls
+          options={this.state}
+          leaveFeedback={this.handlerFeedback}
+        />
+
+        <h2>Statistics</h2>
+        {total === 0 && <p>No feedback given</p>}
+        {total > 0 && (
+        <ul>
+          <li className='value'>Good: <span>{good}</span></li>
+          <li className='value'>Neutral: <span>{neutral}</span></li>
+          <li className='value'>Bad: <span>{bad}</span></li>
+          <li className='value'>Total: <span>{total}</span></li>
+          <li className='value'>Positive feedback: <span>{positivePercentage}%</span></li>
+        </ul>
+        )}
+      </div>
   );
    }
 };
